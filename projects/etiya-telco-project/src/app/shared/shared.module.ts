46ae675aa3744fbeb2e74d225318948d45eda19f
Cagla Ozturk <ozturkcagla041@gmail.com>
2022-08-19 13:31:15 +0300

    search filter eklendi

--- projects/etiya-telco-project/src/app/shared/shared.module.ts
+++ projects/etiya-telco-project/src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OverlayTitleComponent } from './components/overlay-title/overlay-title.component';
+import { SideFilterComponent } from './components/side-filter/side-filter.component';
 
 
 
 @NgModule({
   declarations: [
-    OverlayTitleComponent
+    OverlayTitleComponent,
+    SideFilterComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    OverlayTitleComponent
+    OverlayTitleComponent,
+    SideFilterComponent
   ]
 })
 export class SharedModule { }

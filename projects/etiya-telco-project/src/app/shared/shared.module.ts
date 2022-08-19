b3f1d6ec566031d791a32050eb4f744536a95491
Cagla Ozturk <ozturkcagla041@gmail.com>
2022-08-19 14:37:16 +0300

    Welcome Rose added

--- projects/etiya-telco-project/src/app/shared/shared.module.ts
+++ projects/etiya-telco-project/src/app/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OverlayTitleComponent } from './components/overlay-title/overlay-title.component';
 import { SideFilterComponent } from './components/side-filter/side-filter.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
 
 
 
 @NgModule({
   declarations: [
     OverlayTitleComponent,
-    SideFilterComponent
+    SideFilterComponent,
+    UserInfoComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     OverlayTitleComponent,
-    SideFilterComponent
+    SideFilterComponent,
+    UserInfoComponent
   ]
 })
 export class SharedModule { }
